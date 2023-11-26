1922c8a9e32c7e3bdfda956cd99da2ba430545c8
ratnasree-ghosh <ghoshratnasree817@gmail.com>
2023-11-26 15:39:41 +0530

    baseURL changed

--- client1/src/api/axiosClient.js
+++ client1/src/api/axiosClient.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = process.env.REACT_APP_BACKEND_URL;
+// const baseUrl = 'http://127.0.0.1:5000/api/v1';
+
+const baseUrl = 'https://task-backend-f7ys.onrender.com/api/v1';
+
+
+
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
